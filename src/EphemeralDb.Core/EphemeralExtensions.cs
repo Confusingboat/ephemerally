514c3695072bdde2201908eb44b881241ac5155f
Colton <confusingboat@gmail.com>
2023-05-25 00:07:55 -0500

    reorg

--- src/EphemeralDb.Core/EphemeralExtensions.cs
+++ src/EphemeralDb.Core/EphemeralExtensions.cs
@@ -9,5 +9,8 @@ namespace EphemeralDb;
 public static class EphemeralExtensions
 {
     public static bool IsExpired(this EphemeralMetadata metadata) =>
-        metadata.Expiration.HasValue && metadata.Expiration.Value < DateTimeOffset.UtcNow;
+        IsExpired(metadata, DateTimeOffset.UtcNow);
+
+    internal static bool IsExpired(this EphemeralMetadata metadata, DateTimeOffset now) =>
+        metadata.Expiration.HasValue && metadata.Expiration.Value < now;
 }
