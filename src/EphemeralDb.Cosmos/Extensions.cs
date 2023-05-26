b2885d48d416d20e86973346e2115bb74e5f894d
Colton <confusingboat@gmail.com>
2023-05-26 00:35:40 -0500

    fix some stuff and create basic cosmos container test

--- src/EphemeralDb.Cosmos/Extensions.cs
+++ src/EphemeralDb.Cosmos/Extensions.cs
@@ -9,20 +9,21 @@ public static class Extensions
         EphemeralOptions options = default) =>
         new(client, options);
 
-    public static EphemeralCosmosContainer CreateEphemeralCosmosContainer(
+    public static EphemeralCosmosContainer CreateEphemeralContainerAsync(
         this Database database,
         EphemeralOptions options = default,
         CosmosContainerOptions cosmosContainerOptions = default) =>
         new(database, options, cosmosContainerOptions);
 
-    public static EphemeralCosmosContainer CreateEphemeralCosmosContainer(
-               this EphemeralCosmosDatabase database,
-                      EphemeralOptions options = default,
-                             CosmosContainerOptions cosmosContainerOptions = default) =>
+    public static EphemeralCosmosContainer CreateEphemeralContainerAsync(
+        this EphemeralCosmosDatabase database,
+        EphemeralOptions options = default,
+        CosmosContainerOptions cosmosContainerOptions = default) =>
         new(database.GetAsync, options, cosmosContainerOptions);
 
     public static async Task<bool> ExistsAsync(this Database database) =>
         await database.Client.DatabaseExistsAsync(database.Id);
+
     public static async Task<bool> DatabaseExistsAsync(this CosmosClient client, string databaseId)
     {
         var query = new QueryDefinition("select * from c where c.id = @databaseId")
@@ -33,12 +34,14 @@ public static class Extensions
             .AnyAsync(x => x.Resource.Any());
     }
 
-    public static async Task<bool> ContainerExistsAsync(CosmosClient client, string databaseId)
+    public static async Task<bool> ExistsAsync(this Container container) => 
+        await container.Database.ContainerExistsAsync(container.Id);
+    public static async Task<bool> ContainerExistsAsync(this Database database, string containerId)
     {
-        var query = new QueryDefinition("select * from c where c.id = @databaseId")
-            .WithParameter("@databaseId", databaseId);
+        var query = new QueryDefinition("select * from c where c.id = @containerId")
+            .WithParameter("@containerId", containerId);
 
-        return await client.GetDatabaseQueryIterator<object>(query)
+        return await database.GetContainerQueryIterator<object>(query)
             .ToAsyncEnumerable()
             .AnyAsync(x => x.Resource.Any());
     }
---
+++ tests/EphemeralDb.Cosmos.Tests/EphemeralContainerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EphemeralDb.Cosmos.Tests;
+
+public class EphemeralContainerTests
+{
+    [Test]
+    public async Task Should_create_container_and_tear_it_down()
+    {
+        var client = CosmosEmulator.Client;
+        await using var db = client.CreateEphemeralDatabaseAsync();
+        var sut = (await db.GetAsync()).CreateEphemeralContainerAsync();
+        var container = await sut.GetAsync();
+
+        Assert.That(await container.ExistsAsync(), Is.True);
+
+        await sut.DisposeAsync();
+
+        Assert.That(await container.ExistsAsync(), Is.False);
+    }
+}
