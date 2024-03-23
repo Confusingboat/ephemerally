1b19b22d4f51416513110a121ea3986325923b9f
confusingboat <confusingboat@gmail.com>
2024-03-23 17:59:30 -0500

    remove unused usings

--- tests/Ephemerally.Redis.Tests/Playground.cs
+++ tests/Ephemerally.Redis.Tests/Playground.cs
@@ -1,9 +1,4 @@
 ﻿using Ephemerally.Redis.xUnit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using Ephemerally.Tests;
