65eac7072894d71c650c46a700fba0506e728674
confusingboat <confusingboat@gmail.com>
2024-03-29 01:42:50 -0500

    test: add test for redis database fixture

---
+++ tests/Ephemerally.Redis.Tests/Fixtures/RedisDatabaseFixtureTests.cs
@@ -0,0 +1,25 @@
+﻿using Ephemerally.Redis.Xunit;
+using Shouldly;
+
+namespace Ephemerally.Redis.Tests.Fixtures;
+
+[Collection(RedisDatabaseFixtureTestCollection.Name)]
+public class RedisDatabaseFixtureTests(RedisDatabaseFixture<RedisDatabaseFixtureTestCollectionFixture> databaseFixture)
+    : IClassFixture<RedisDatabaseFixture<RedisDatabaseFixtureTestCollectionFixture>>
+{
+    [Fact]
+    public void Database_should_not_be_null()
+    {
+        databaseFixture.Database.ShouldNotBeNull();
+    }
+}
+
+[CollectionDefinition(Name)]
+public class RedisDatabaseFixtureTestCollection
+    : ICollectionFixture<RedisDatabaseFixtureTestCollectionFixture>
+{
+    public const string Name = nameof(RedisDatabaseFixtureTestCollection);
+}
+
+public class RedisDatabaseFixtureTestCollectionFixture
+    : PooledEphemeralRedisMultiplexerFixture<RedisInstanceFixture7>;
