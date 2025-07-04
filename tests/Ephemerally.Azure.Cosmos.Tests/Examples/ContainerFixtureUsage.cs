754e554572514adf295482e8b60b5fb91bb2e9c4
confusingboat <confusingboat@gmail.com>
2025-07-03 22:08:08 -0500

    add usage examples

---
+++ tests/Ephemerally.Azure.Cosmos.Tests/Examples/ContainerFixtureUsage.cs
@@ -0,0 +1,20 @@
+﻿using Ephemerally.Azure.Cosmos.Xunit;
+using Xunit;
+
+namespace Ephemerally.Azure.Cosmos.Tests.Examples;
+
+public class ContainerFixtureUsageExampleTests(EphemeralCosmosContainerFixture fixture)
+    : IClassFixture<EphemeralCosmosContainerFixture>
+{
+    [Fact(Skip = "Example")]
+    public async Task TestUsingContainer()
+    {
+        // Do something with the container
+        await fixture.Container.CreateItemAsync(
+            new
+            {
+                id = "12345",
+                message = "Hello World!"
+            });
+    }
+}
---
+++ tests/Ephemerally.Azure.Cosmos.Tests/Examples/DatabaseFixtureUsage.cs
@@ -0,0 +1,28 @@
+﻿using Ephemerally.Azure.Cosmos.Xunit;
+using Xunit;
+
+namespace Ephemerally.Azure.Cosmos.Tests.Examples;
+
+public class DatabaseFixtureUsageExampleTests(EphemeralCosmosDatabaseFixture fixture)
+    : IClassFixture<EphemeralCosmosDatabaseFixture>
+{
+    [Fact(Skip = "Example")]
+    public async Task TestUsingDatabase()
+    {
+        // Ephemeral databases go nicely with ephemeral containers to keep tests isolated
+        await using var container = await fixture.Database.CreateEphemeralContainerAsync();
+    }
+}
+
+public class CustomEphemeralDatabaseFixture() : EphemeralCosmosDatabaseFixture(new CustomCosmosClientFixture());
+
+public class CustomDatabaseFixtureWithCustomClientUsageExampleTests(CustomEphemeralDatabaseFixture fixture)
+    : IClassFixture<CustomEphemeralDatabaseFixture>
+{
+    [Fact(Skip = "Example")]
+    public async Task TestUsingDatabase()
+    {
+        // Ephemeral databases go nicely with ephemeral containers to keep tests isolated
+        await using var container = await fixture.Database.CreateEphemeralContainerAsync();
+    }
+}
